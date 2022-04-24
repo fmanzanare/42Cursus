76a71606b0e69acacb9c91ad5175618a94013eaf
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2022-04-24 17:40:40 +0200

    Create ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmanzana < fmanzana@student.42malaga.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/24 16:09:24 by fmanzana          #+#    #+#             */
+/*   Updated: 2022/04/24 17:40:08 by fmanzana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+	char	*dst;
+
+	while (*s1 && ft_strchr(set, *s1) != 0)
+		s1++;
+	len = ft_strlen(s1);
+	while (len && s1[len - 1] && ft_strchr(set, s1[len - 1]) != 0)
+		len--;
+	dst = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dst)
+		return (0);
+	ft_memcpy(dst, s1, len);
+	dst[len] = '\0';
+	return (dst);
+}
