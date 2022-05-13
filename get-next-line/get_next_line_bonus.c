7519b685ae1ad21111eaecf9d6c8683518cf06d2
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2022-05-13 13:43:13 +0200

    Update get_next_line_bonus.c

--- get-next-line/get_next_line_bonus.c
+++ get-next-line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 int		ft_fillmem(char **mem, int fd);
 void	ft_cleanmem(char **mem);
