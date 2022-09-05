f42351db74006ecd7378252cdaf9ce35cad4d919
= <=>
2022-09-05 16:33:22 +0200

    pj moving up OK

--- so_long/hooks_functions.c
+++ so_long/hooks_functions.c
@@ -6,7 +6,7 @@
 /*   By: fmanzana <fmanzana@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 15:29:39 by fmanzana          #+#    #+#             */
-/*   Updated: 2022/09/05 16:16:11 by fmanzana         ###   ########.fr       */
+/*   Updated: 2022/09/05 16:32:31 by fmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ static void	find_pj_pos(t_data *data)
 		data->map.pj_pos++;
 		i++;
 	}
-	printf("%i\n\n", data->map.pj_pos);
 }
 
 static void	move_pj_down(t_data *data)
@@ -32,14 +31,26 @@ static void	move_pj_down(t_data *data)
 
 	find_pj_pos(data);
 	i = data->map.pj_pos + data->map.width + 1;
-	if (data->map.str_map[i] != '1' && data->map.str_map[i] != 'P' && data->map.str_map[i] != '\n')
+	if (data->map.str_map[i] != '1' && data->map.str_map[i] != '\n' && data->map.str_map[i] != 'E')
 	{
 		data->map.str_map[data->map.pj_pos] = '0';
 		data->map.str_map[i] = 'P';
 		pre_print_map(data);
 	}
-	printf("%i\n", data->map.width);
-	printf("%s\n", data->map.str_map);
+}
+
+static void	move_pj_up(t_data *data)
+{
+	int		i;
+
+	find_pj_pos(data);
+	i = data->map.pj_pos - data->map.width - 1;	
+	if (data->map.str_map[i] != '1' && data->map.str_map[i] != '\n' && data->map.str_map[i] != 'E')
+	{
+		data->map.str_map[data->map.pj_pos] = '0';
+		data->map.str_map[i] = 'P';
+		pre_print_map(data);
+	}
 }
 
 static void	close_window(t_data *data)
@@ -58,5 +69,7 @@ int	input(int keycode, void *ptr)
 		close_window(data);
 	else if (keycode == DOWN)
 		move_pj_down(data);
+	else if (keycode == UP)
+		move_pj_up(data);
 	return (0);
 }
