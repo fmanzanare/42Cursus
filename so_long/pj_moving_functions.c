2a831688c4f92cb542180ec9cfa99d3e86d80108
= <=>
2022-09-05 18:28:17 +0200

    collectable things

--- so_long/pj_moving_functions.c
+++ so_long/pj_moving_functions.c
@@ -6,7 +6,7 @@
 /*   By: fmanzana <fmanzana@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 16:42:14 by fmanzana          #+#    #+#             */
-/*   Updated: 2022/09/05 16:57:42 by fmanzana         ###   ########.fr       */
+/*   Updated: 2022/09/05 18:26:56 by fmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,11 @@ void	move_pj_down(t_data *data)
 	if (data->map.str_map[i] != '1' && data->map.str_map[i] != '\n'
 		&& data->map.str_map[i] != 'E')
 	{
+		if (data->map.str_map[i] == 'C')
+		{
+			data->map.collect -= 1;
+			printf("%i\n", data->map.collect);
+		}
 		data->map.str_map[data->map.pj_pos] = '0';
 		data->map.str_map[i] = 'P';
 		pre_print_map(data);
@@ -49,6 +54,11 @@ void	move_pj_up(t_data *data)
 	if (data->map.str_map[i] != '1' && data->map.str_map[i] != '\n'
 		&& data->map.str_map[i] != 'E')
 	{
+		if (data->map.str_map[i] == 'C')
+		{
+			data->map.collect -= 1;
+			printf("%i\n", data->map.collect);
+		}
 		data->map.str_map[data->map.pj_pos] = '0';
 		data->map.str_map[i] = 'P';
 		pre_print_map(data);
@@ -64,12 +74,22 @@ void	move_pj_right(t_data *data)
 	if (data->map.str_map[i] != '1' && data->map.str_map[i] != '\n'
 		&& data->map.str_map[i] != 'E')
 	{
+		if (data->map.str_map[i] == 'C')
+		{
+			data->map.collect -= 1;
+			printf("%i\n", data->map.collect);
+		}
 		data->map.str_map[data->map.pj_pos] = '0';
+		data->map.str_map[i] = '0';
+		pre_print_map(data);
 		data->map.str_map[i] = 'P';
 		pre_print_map(data);
 	}
+	else if (data->map.str_map[i] == 'E' && data->map.collect == 0)
+	{
+	// Animacion puerta abierta + finalizacion programa
+	}
 }
-
 void	move_pj_left(t_data *data)
 {
 	int		i;
@@ -79,6 +99,11 @@ void	move_pj_left(t_data *data)
 	if (data->map.str_map[i] != '1' && data->map.str_map[i] != '\n'
 		&& data->map.str_map[i] != 'E')
 	{
+		if (data->map.str_map[i] == 'C')
+		{
+			data->map.collect -= 1;
+			printf("%i\n", data->map.collect);
+		}
 		data->map.str_map[data->map.pj_pos] = '0';
 		data->map.str_map[i] = 'P';
 		pre_print_map(data);
