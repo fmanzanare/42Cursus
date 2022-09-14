8d4b918e0122ae4ecad6c6ea4bd77fd48f08d925
Fernando Manzanares <fernando_mzmt@hotmail.com>
2022-09-14 13:35:22 +0200

    Makefile listo

--- pipex/sources/main_pipex.c
+++ pipex/sources/main_pipex.c
@@ -6,7 +6,7 @@
 /*   By: fmanzana <fmanzana@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 12:40:05 by fmanzana          #+#    #+#             */
-/*   Updated: 2022/09/14 13:20:47 by fmanzana         ###   ########.fr       */
+/*   Updated: 2022/09/14 13:32:36 by fmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(void)
 {
-	int	i = 50;
-
-	ft_printf("Hello World %i\n", i);
+	char	*str = "Hola Mundo";
+	
+	printf("len str: %li\n", ft_strlen(str));
 	return (0);
 }
