5f61e63a044d258955d5a607b4be8edf06c44a40
= <=>
2022-12-22 13:22:12 +0100

    new start

--- push_swap/src/stackB.c
+++ push_swap/src/stackB.c
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   stackB.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmanzana <fmanzana@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/17 12:56:40 by fmanzana          #+#    #+#             */
-/*   Updated: 2022/12/22 13:18:25 by fmanzana         ###   ########.fr       */
+/*   Created: 2022/12/22 12:54:56 by fmanzana          #+#    #+#             */
+/*   Updated: 2022/12/22 12:55:06 by fmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include"./libft_plus/libft.h"
-
-typedef struct list
-{
-    int     num;
-    list    *next;
-} list;
-
-#endif
