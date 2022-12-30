ec83e4b170ca5dc4d0b47445387a325bdb84397b
= <=>
2022-12-30 14:24:28 +0100

    checker done. Makefile not updated

--- push_swap/src_bonus/checker.c
+++ push_swap/src_bonus/checker.c
@@ -6,25 +6,53 @@
 /*   By: fmanzana <fmanzana@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:06:01 by fmanzana          #+#    #+#             */
-/*   Updated: 2022/12/30 13:16:17 by fmanzana         ###   ########.fr       */
+/*   Updated: 2022/12/30 14:07:12 by fmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	command_finder(char **cmd)
+static int	command_finder(char *cmd, t_stack **stk_a, t_stack **stk_b)
 {
-
+	if (!ft_strncmp("sa\n", cmd, 4))
+		sa_act(stk_a);
+	else if (!ft_strncmp("sb\n", cmd, 4))
+		sb_act(stk_b);
+	else if (!ft_strncmp("ra\n", cmd, 4))
+		ra_act(stk_a);
+	else if (!ft_strncmp("rb\n", cmd, 4))
+		rb_act(stk_b);
+	else if (!ft_strncmp("rr\n", cmd, 4))
+		rr_act(stk_a, stk_b);
+	else if (!ft_strncmp("rra\n", cmd, 5))
+		rra_act(stk_a);
+	else if (!ft_strncmp("rrb\n", cmd, 5))
+		rrb_act(stk_b);
+	else if (!ft_strncmp("rrr\n", cmd, 5))
+		rrr_act(stk_a, stk_b);
+	else if (!ft_strncmp("pb\n", cmd, 4))
+		pb_act(stk_a, stk_b);
+	else if (!ft_strncmp("pa\n", cmd, 4))
+		pa_act(stk_a, stk_b);
+	else
+		return (0);
+	return (1);
 }
 
-void	command_reader(t_stack ***stk_a, t_stack **stk_b)
+static void	command_reader(t_stack **stk_a, t_stack **stk_b)
 {
 	char	*cmd;
 
 	cmd = get_next_line(0);
 	while (cmd)
 	{
-
+		if (!command_finder(cmd, stk_a, stk_b))
+		{
+			free_stk(stk_b);
+			ft_errexit(*stk_a);
+		}
+		free(cmd);
+		cmd = get_next_line(0);
 	}
 }
 
@@ -41,8 +69,12 @@ int	main(int argc, char **args)
 	stk_b = NULL;
 	stk_a = args_splitter_and_stk_filler(args);
 	check_duplicates(stk_a);
-
-	free_stk(stk_a);
-	free_stk(stk_b);
+	command_reader(&stk_a, &stk_b);
+	if (check_sorted(stk_a))
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
+	free_stk(&stk_a);
+	free_stk(&stk_b);
 	return (0);
 }
